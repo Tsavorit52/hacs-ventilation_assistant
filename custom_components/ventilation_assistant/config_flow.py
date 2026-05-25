85fb318e4bce6308b9982483520623b884912ba4
Tsavorit52 <tsavorit52@gmail.com>
2026-05-25 12:58:15 +0200

    Fix config flow handler and add strings.json for proper localization

--- custom_components/ventilation_assistant/config_flow.py
+++ custom_components/ventilation_assistant/config_flow.py
@@ -16,58 +16,62 @@ from .const import (
 )
 
 
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_INSIDE_TEMPERATURE): selector.selector(
+            {
+                "entity": {
+                    "domain": "sensor",
+                    "device_class": "temperature",
+                }
+            }
+        ),
+        vol.Required(CONF_INSIDE_HUMIDITY): selector.selector(
+            {
+                "entity": {
+                    "domain": "sensor",
+                    "device_class": "humidity",
+                }
+            }
+        ),
+        vol.Required(CONF_INSIDE_CO2): selector.selector(
+            {
+                "entity": {
+                    "domain": "sensor",
+                }
+            }
+        ),
+        vol.Required(CONF_OUTSIDE_TEMPERATURE): selector.selector(
+            {
+                "entity": {
+                    "domain": "sensor",
+                    "device_class": "temperature",
+                }
+            }
+        ),
+        vol.Required(CONF_OUTSIDE_HUMIDITY): selector.selector(
+            {
+                "entity": {
+                    "domain": "sensor",
+                    "device_class": "humidity",
+                }
+            }
+        ),
+    }
+)
+
+
 class VentilationAssistantConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Ventilation Assistant."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
+        """Handle a flow initiated by the user."""
         if user_input is not None:
             return self.async_create_entry(title="Ventilation Assistant", data=user_input)
 
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_INSIDE_TEMPERATURE): selector.selector(
-                    {
-                        "entity": {
-                            "domain": "sensor",
-                            "device_class": "temperature",
-                        }
-                    }
-                ),
-                vol.Required(CONF_INSIDE_HUMIDITY): selector.selector(
-                    {
-                        "entity": {
-                            "domain": "sensor",
-                            "device_class": "humidity",
-                        }
-                    }
-                ),
-                vol.Required(CONF_INSIDE_CO2): selector.selector(
-                    {
-                        "entity": {
-                            "domain": "sensor",
-                        }
-                    }
-                ),
-                vol.Required(CONF_OUTSIDE_TEMPERATURE): selector.selector(
-                    {
-                        "entity": {
-                            "domain": "sensor",
-                            "device_class": "temperature",
-                        }
-                    }
-                ),
-                vol.Required(CONF_OUTSIDE_HUMIDITY): selector.selector(
-                    {
-                        "entity": {
-                            "domain": "sensor",
-                            "device_class": "humidity",
-                        }
-                    }
-                ),
-            }
-        )
-
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
+            data_schema=CONFIG_SCHEMA,
         )
